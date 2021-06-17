6c5b927dac176db15b8d50c7f3e5515d2369c212
raeel97 <hecate101@gmail.com>
2021-06-17 15:21:26 +0000

    added in flip function and function to check if cards match

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,10 @@ const cardList = [
 ]
 
 const grid = document.querySelector(".grid")
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
 
 // Create card layout 
 function createBoard(){
@@ -26,6 +30,37 @@ function createBoard(){
         grid.appendChild(card)
     }
 }
+// Check cards for matches
+function checkForMatch(){
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosenId[0] === cardsChosenId[1]){
+        alert('You found a match')
+        cards[OptionOneId].setAttribute('src', './assets/images/matched.png')
+        cards[OptionTwoId].setAttribute('src', './assets/images/matched.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[OptionOneId].setAttribute('src', './assets/images/blank.png')
+        cards[OptionTwoId].setAttribute('src', './assets/images/blank.png')
+        alert('Sorry try again!')
+    } 
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay
+}
+// Create flip function to flip cards
+function flipCard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardList[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardList[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkForMatch, 500)
+    }  
+}
+
 
 createBoard()
 })
+
