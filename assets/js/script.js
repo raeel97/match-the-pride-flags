88bb323c86ac44c666ad6b4363e3c1ae4a4d1e86
raeel97 <hecate101@gmail.com>
2021-06-17 13:47:45 +0000

    created card layout, used CSS to position and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const cardList = [
-    { cardName: 'pride-flag', cardImage: 'pride-flag.png' }, 
-    { cardName: 'bi-flag', cardImage: 'bi-flag.png' }, 
-    { cardName: 'intersex-flag', cardImage: 'intersex-flag.png' }
-    { cardName: 'asexual-flag', cardImage: 'asexual-flag.png' }, 
-    { cardName: 'pansexual-flag', cardImage: 'pansexual-flag.png' }, 
-    { cardName: 'trans-flag', cardImage: 'trans-flag.png' }
+    { name: 'pride-flag', img: '../images/pride-flag.png' }, 
+    { name: 'pride-flag', img: '../images/pride-flag.png' }, 
+    { name: 'bi-flag', img: '../images/bi-flag.png' }, 
+    { name: 'bi-flag', img: '../images/bi-flag.png' }, 
+    { name: 'intersex-flag', img: '../images/intersex-flag.png' },
+    { name: 'intersex-flag', img: '../images/intersex-flag.png' },
+    { name: 'asexual-flag', img: '../images/asexual-flag.png' }, 
+    { name: 'asexual-flag', img: '../images/asexual-flag.png' }, 
+    { name: 'pansexual-flag', img: '../images/pansexual-flag.png' },
+    { name: 'pansexual-flag', img: '../images/pansexual-flag.png' },  
+    { name: 'trans-flag', img: '../images/trans-flag.png' },
+    { name: 'trans-flag', img: '../images/trans-flag.png' },
 ]
+
+const grid = document.querySelector(".grid")
+
+// Create card layout 
+function createBoard(){
+    for (let i = 0; i < cardList.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', './assets/images/blank.png')
+        card.setAttribute('data-id', i)
+        grid.appendChild(card)
+    }
+}
+
+createBoard()
+})
