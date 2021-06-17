ec79d3aaf695d3ec1cc6f3b64af82e7a278a919e
raeel97 <hecate101@gmail.com>
2021-06-17 12:14:13 +0000

    added memory game card tiles in array

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const cardList = [
+    { cardName: 'pride-flag', cardImage: 'pride-flag.png' }, 
+    { cardName: 'bi-flag', cardImage: 'bi-flag.png' }, 
+    { cardName: 'intersex-flag', cardImage: 'intersex-flag.png' }
+    { cardName: 'asexual-flag', cardImage: 'asexual-flag.png' }, 
+    { cardName: 'pansexual-flag', cardImage: 'pansexual-flag.png' }, 
+    { cardName: 'trans-flag', cardImage: 'trans-flag.png' }
+]
