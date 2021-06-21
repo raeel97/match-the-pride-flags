b158be9031a7e9d6f23e85e1fa64737f30d8d2f8
raeel97 <hecate101@gmail.com>
2021-06-21 17:53:44 +0000

    added comments to javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    // Created an array with 2 tiles for each flag 
     const cardList = [
         {
             name: 'pride-flag',
@@ -50,15 +52,19 @@ document.addEventListener('DOMContentLoaded', () => {
             img: './assets/images/trans-flag.png'
         },
     ]
-    cardList.sort(() => 0.5 - Math.random())
+    cardList.sort(() => 0.5 - Math.random()) //Sort method is used to in conjunction with the Math.random function to shuffle the tiles
 
-    const grid = document.querySelector(".grid")
+    const grid = document.querySelector(".grid") //Selects the div in the html with .grid class and assigns it to the variable grid
     const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
+    let cardsChosen = [] // Creates a variable named cardsChosen and assigns it a value of an empty array
+    let cardsChosenId = [] // Creates a variable named cardsChosenId and assigns it a value of an empty array
+    let cardsWon = [] // Creates a variable named cardsWon and assigns it a value of an empty array
 
-    // Create card layout 
+    /* Create card layout by using a for loop to iterate through the cardList array and create a img element in the html
+     with a src attribute that links to my blank tile photo and a data id attribute with a numerical value for each img element. 
+     Then it "listens" for the click of the img and in response calls the flipCard function. The for loop then creates a child node
+     of card for the parent node "grid".
+    */
     function createBoard() {
         for (let i = 0; i < cardList.length; i++) {
             const card = document.createElement('img')
@@ -68,37 +74,49 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(card)
         }
     }
-    // Check cards for matches
-    function checkForMatch() {
-        const cards = document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
 
+    // Check cards for matches. This is done by creating a function called checkForMatch. 
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img') // A variable of cards is assigned the value of all img elements
+        const optionOneId = cardsChosenId[0] // A variable of optionOneId is assigned the value of the first item in the cardsChosenId array. This is the first card that has been clicked
+        const optionTwoId = cardsChosenId[1] // A variable of optionTwoId is assigned the value of the second item in the cardsChosenId array. This is the second card that has been clicked
+
+        // An if statment in the if else statement is used to determine if the same image has been clicked twice. This is achieved by checking 
+        // if the values of optionOneId and optionTwoId are strictly equal (The same item in the array), if its true, the card is 'flipped' back 
+        // over by reassigning it a blank image again. An alert is then sent to the browser informing the user that they clicked the same image
         if (optionOneId == optionTwoId) {
             cards[optionOneId].setAttribute('src', './assets/images/blank.png')
             cards[optionTwoId].setAttribute('src', './assets/images/blank.png')
-            alert('You have clicked the same image')
-        }
+            alert('You have clicked the same image gurl!')
+        } //if the if part of the if else statement is false, the next part the code run is the else if which checks to see if the tiles have the same value (name)
+        // if its true, an alert is sent to the browser informing the user that they have a matching pair and both cardsChosen[0] and CardsChosen[1] have their src 
+        // attribute reassigned to a matched image. It then removes the event listener thats waiting for the user to click on the tile so it can call the flip function. 
+        // This prevents the user from selecting matched cards. The names in the cardsChosen array are then pished into the cardsWon array. 
         else if (cardsChosen[0] === cardsChosen[1]) {
-            alert('You found a match')
+            alert('You found a match, shantay you stay!')
             cards[optionOneId].setAttribute('src', './assets/images/matched.png')
             cards[optionTwoId].setAttribute('src', './assets/images/matched.png')
             cards[optionOneId].removeEventListener('click', flipCard)
             cards[optionTwoId].removeEventListener('click', flipCard)
             cardsWon.push(cardsChosen)
-        } else {
+        } else { // If the if and else if are false, the else part of the statmenet will run signally that there is a mismatch in the selected tiles. This will result 
+            //if its true, the card is 'flipped' back over by reassigning it a blank image again and alerting the user of the mismatch. 
             cards[optionOneId].setAttribute('src', './assets/images/blank.png')
             cards[optionTwoId].setAttribute('src', './assets/images/blank.png')
-            alert('Sorry try again!')
+            alert('Mismatch, sashay away!')
         }
-        cardsChosen = []
-        cardsChosenId = []
+        cardsChosen = [] // A variable of cardsChosen is assigned the value of an empty array
+        cardsChosenId = [] // A variable of cardsChosenId is assigned the value of an empty array
         resultDisplay.textContent = cardsWon.length
         if (cardsWon.length === cardList.length/2) {
-            resultDisplay.textContent = 'Congratulations, you found them all!'
+            resultDisplay.textContent = 'Congratulations, you found them all!' 
         }
     }
-    // Create flip function to flip cards
+    // Create flip function to flip cards. This is done by assigning a variable of cardId with a value of the data-id attribute in the window
+    // cardId datatype value in the cardList array is then utilized as a key and pushed into the cardChosen array. The cardId value is then pushed 
+    // into the cardsChosenId array.
+    //
+    // The checkForMatch function is then called if the values in the cardsChosen array are equal to 2. it takes 500 milliseconds for this to execute
     function flipCard() {
         let cardId = this.getAttribute('data-id')
         cardsChosen.push(cardList[cardId].name)
@@ -110,7 +128,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    createBoard()
+    createBoard() // Calling the createBoard function
 })
-
-let hamburger = document.getElementById('hamburger').addEventListener('click')
